f66b70d6d60c7ac42d4c1b9c4e8761b6d8a0738c
Nathaniel Furniss <nlfurniss@gmail.com>
2021-06-03 10:22:37 -0700

    Add ember-cli-github-pages

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -44,6 +44,8 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
+    ENV.locationType = 'hash';
+    ENV.rootURL = '/ember-simple-wormhole/';
     // here you can enable a production-specific feature
   }
 
